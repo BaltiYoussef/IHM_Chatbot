7d3a593c78036d79baed04aa6cc86d363822ca1e
Thomas Vendramini <thomas.vendramini@sfr.fr>
2022-03-11 16:42:12 +0100

    img qui marche

--- js/affichage_articles.js
+++ js/affichage_articles.js
@@ -8,8 +8,9 @@
         while (j < ourData.length) {
             document.getElementById("description" + j).innerHTML = ourData[j].description;
             document.getElementById("prix" + j).innerHTML = ourData[j].prix;
-            document.getElementById("image" + j).innerHTML = ourData[j].image;
-            console.log(ourData[j].image)
+            document.getElementById("img" + j).setAttribute('xlink:href', ourData[j].image);
+            document.getElementById("nom" + j).innerHTML = ourData[j].name;
+
 
         }
     };
