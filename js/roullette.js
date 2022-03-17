6f8453bcbc628553ea31d0db42a88d6ef75ffe75
Thomas Vendramini <thomas.vendramini@sfr.fr>
2022-03-17 17:23:17 +0100

    test

--- js/roullette.js
+++ js/roullette.js
@@ -3,9 +3,15 @@ img.addEventListener('click', onClick, false);
 
 
 function onClick() {
+    const music = new Audio('./../img/audio.wav');
+    music.play();
+    music.loop = true;
+    music.playbackRate = 2;
+    music.pause();
+
     this.removeAttribute('style');
 
-    var deg = 500 + Math.round(Math.random() * 500);
+    var deg = 500 + Math.round(Math.random() * 1500);
 
     var css = '-webkit-transform: rotate(' + deg + 'deg);';
 
