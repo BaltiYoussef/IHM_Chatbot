9cb7390134ec86ba875f325d7c0700f9ed0128e1
Youssef BALTI <youssefbalti96@gmail.com>
2022-03-17 23:54:16 +0100

    ajout son squidgame et qlq avis

--- brain/script.js
+++ brain/script.js
@@ -1,5 +1,15 @@
+var sfx = {
+   push: new Howl({
+      src: [
+         './../IHM_Chatbot/audio.mp3',
+      ]
+   })
+}
+
 function setup() {
 	chatbot.loadFiles(['brain/bot.rive']);
 }
 
+window.onload = sfx.push.play();
 window.onload = setup;
+
