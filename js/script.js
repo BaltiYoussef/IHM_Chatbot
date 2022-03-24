614498995da7bea8d677b1a7b5f292066b9be8df
Youssef BALTI <youssefbalti96@gmail.com>
2022-03-24 11:48:25 +0100

    Supression des scripts innecessaires

--- js/script.js
+++ js/script.js
@@ -1,15 +1,6 @@
-var sfx = {
-   push: new Howl({
-      src: [
-         './../IHM_Chatbot/audio.mp3',
-      ]
-   })
-}
-
 function setup() {
 	chatbot.loadFiles(['brain/bot.rive']);
 }
 
-window.onload = sfx.push.play();
 window.onload = setup;
 
