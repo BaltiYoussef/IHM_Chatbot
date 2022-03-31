df12f050249bf9c462b2c3c8d10a7ef9cef8f592
Youssef BALTI <55838785+BaltiYoussef@users.noreply.github.com>
2022-03-31 20:43:23 +0200

    Update affichage_articles.js

--- js/affichage_articles.js
+++ js/affichage_articles.js
@@ -1,6 +1,6 @@
 (function() {
     var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', './../IHM_Chatbot/JSON/description.json');
+    ourRequest.open('GET', 'https://github.com/BaltiYoussef/IHM_Chatbot.github.io/blob/main/JSON/description.json');
     ourRequest.send();
     ourRequest.onload = function() {
         var ourData = JSON.parse(ourRequest.responseText);
